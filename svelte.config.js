2eb0a799f77a650a34261d0e1cfbdfb4a60999ba
Aleksandar  Brayanov <aleksandar@chexvisual.co.uk>
2023-08-23 13:06:09 +0100

    updated default production configuration setup

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,13 @@
 //import adapter from '@sveltejs/adapter-auto';
 //import adapter from '@sveltejs/adapter-netlify';
-import adapter from '@sveltejs/adapter-node';
-//import { vitePreprocess } from '@sveltejs/kit/vite';
+//import adapter from '@sveltejs/adapter-node';
 import adapter from '@sveltejs/adapter-static';
+
+import { vitePreprocess } from '@sveltejs/kit/vite';
  
 export default {
- preprocess: vitePreprocess(),
+  preprocess: [vitePreprocess()],
   kit: {
-    adapter: adapter()
-    
- /* 
     //SSG Adapter 
     adapter: adapter({
       // default options are shown. On some platforms
@@ -19,6 +17,24 @@ export default {
       fallback: undefined,
       precompress: false,
       strict: false
+    })
+   /* 
+    // Node Server Adapter 
+    adapter: adapter({
+      out: 'build-node',
+      precompress: false
+    })
+
+    // Netlify Adapter
+    adapter: adapter({
+      // if true, will create a Netlify Edge Function rather
+      // than using standard Node-based functions
+      edge: false,
+ 
+      // if true, will split your app into multiple functions
+      // instead of creating a single one for the entire app.
+      // if `edge` is true, this option cannot be used
+      split: false
     })
 */ 
   }
